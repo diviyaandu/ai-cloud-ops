f9c863babf28c67d0b80cd66b2ef8f8dd8b1075b
diviyaandu <divyendupradeep0@gmail.com>
2026-05-14 21:16:39 +0530

    Added API routes

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+from random import randint
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "AI Cloud Ops Running"}
+    return {
+        "message": "AI Cloud Ops Running"
+    }
+
+@app.get("/metrics")
+def metrics():
+    return {
+        "cpu": randint(20, 95),
+        "memory": randint(30, 90),
+        "disk": randint(40, 85)
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
